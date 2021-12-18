03f537a83465311b1a91bba0962ab3fb9eadf1db
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-18 22:56:38 +0530

    Show the CalculatorView on the main screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import CalculatorView from "./components/CalculatorView"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = () => {
+    return <CalculatorView height={ '100vh' } width={ '100vw' } />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App
