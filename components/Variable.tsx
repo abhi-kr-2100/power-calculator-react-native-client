8840217eb2b263e444d24c89d8c1f18ba6320729
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-12-23 14:28:21 +0530

    Make the variable component more customizable

--- components/Variable.tsx
+++ components/Variable.tsx
@@ -1,65 +1,80 @@
-import { View, Text, StyleProp, ViewStyle, ColorValue } from "react-native"
+import { View, Text, StyleProp, ViewStyle, ColorValue, TextStyle } from "react-native"
 
 
 export type VariablePropsType = {
-    height?: string | number | undefined
+    width: string | number | undefined
+    height: string | number | undefined
 
     name: string
     value: number
 
-    buttonBgColor: ColorValue | undefined
+    textBgColor: ColorValue | undefined
+    useButtonBgColor: ColorValue | undefined
+    deleteButtonBgColor: ColorValue | undefined
+
+    borderWidth: number
 }
 
 
 const Variable = (props: VariablePropsType) => {
     const topStyle: StyleProp<ViewStyle> = {
-        width: '100%',
+        width: props.width,
         height: props.height,
 
         flexDirection: "row",
         alignItems: 'center',
-        justifyContent: 'space-evenly',
 
-        backgroundColor: 'wheat',
+        backgroundColor: props.textBgColor,
 
-        borderBottomWidth: 1
+        borderBottomWidth: props.borderWidth
     }
 
-    const childrenStyle = {
+    const variableInfoOuterStyle: StyleProp<ViewStyle> = {
         width: '35%',
         height: '100%',
 
-        textAlign: 'center',
         justifyContent: 'center'
+    }
+
+    const variableInfoInnerStyle: StyleProp<TextStyle> = {
+        textAlign: 'center'
+    }
+
+    const useButtonOuterStyle: StyleProp<ViewStyle> = {
+        width: '20%',
+        height: '100%',
+
+        backgroundColor: props.useButtonBgColor,
+
+        justifyContent: 'center'
+    }
+
+    const deleteButtonOuterStyle: StyleProp<ViewStyle> = {
+        width: '10%',
+        height: '100%',
+
+        backgroundColor: props.deleteButtonBgColor,
+        justifyContent: 'center'
+    }
+
+    const variableActionButtonsInnerStyle: StyleProp<TextStyle> = {
+        textAlign: 'center'
     }
 
     return (
         <View style={ topStyle }>
-            <View style={ childrenStyle }>
-                <Text style={{ textAlign: 'center' }}>{ props.name }</Text>
+            <View style={ variableInfoOuterStyle }>
+                <Text style={ variableInfoInnerStyle }>{ props.name }</Text>
             </View>
-            <View style={ childrenStyle }>
-                <Text style={{ textAlign: 'center' }}>{ props.value }</Text>
-            </View>
-
-            <View style={{
-                width: '20%',
-                height: '100%',
-                backgroundColor: props.buttonBgColor,
-
-                justifyContent: 'center'
-            }}>
-                <Text style={{ textAlign: 'center' }}>USE</Text>
+            <View style={ variableInfoOuterStyle }>
+                <Text style={ variableInfoInnerStyle }>{ props.value }</Text>
             </View>
 
-            <View style={{
-                width: '10%',
-                height: '100%',
-                backgroundColor: 'red',
-
-                justifyContent: 'center'
-            }}>
-                <Text style={{ textAlign: 'center' }}>X</Text>
+            <View style={ useButtonOuterStyle }>
+                <Text style={ variableActionButtonsInnerStyle }>USE</Text>
+            </View>
+            <View style={ deleteButtonOuterStyle }>
+                <Text style={ variableActionButtonsInnerStyle }>X</Text>
             </View>
         </View>
     )
